c353fffa3055530a067f885abb0ea650b415b97d
imindMan <khanhdonglethanhtong7b2@gmail.com>
2022-09-04 10:46:18 +0700

    Make the snake moves

--- snake.c
+++ snake.c
@@ -5,7 +5,8 @@
 
 // initialize some structure and functions
 
-struct Screen {
+struct Screen
+{
 	int width;
 	int height;
 	char *display;
@@ -15,251 +16,490 @@ struct Screen generate_screen(int width, int height);
 
 void display(struct Screen *scr);
 
-void draw_on_the_screen(struct Screen *scr, int x, int y, char content) {
+void draw_on_the_screen(struct Screen *scr, int x, int y, char content)
+{
 
-	if (scr->display[(y * (scr->width) + x)] == '\n') {
-		scr->display[(y * (scr->width) + x) - 1] = content; 
-	} else {
+	if (scr->display[(y * (scr->width) + x)] == '\n')
+	{
+		scr->display[(y * (scr->width) + x) - 1] = content;
+	}
+	else
+	{
 		scr->display[(y * (scr->width) + x)] = content;
 	}
-	
 }
 
-
-struct Snake {
+struct Snake
+{
 	int start_x;
 	int start_y;
 	int blocks;
 	char *direction;
 };
 
-void get_Snake(struct Snake *snake, int start_x, int start_y, int blocks);
-void draw_snake(struct Snake *snake, struct Screen *screen);
-void move_snake(struct Snake *snake, struct Screen *screen, char *direction);
-void delete_snake(struct Snake *snake, struct Screen *screen);
+void get_snake(struct Snake *snake, int start_x, int start_y, int blocks);
+void draw_snake(struct Snake *snake, struct Screen *screen, int col_or_not)
+{
 
+	if (col_or_not == 0)
+	{
+		int x = snake->start_x;
+		for (int i = 0; i < snake->blocks; i++)
+		{
+			draw_on_the_screen(screen, x, snake->start_y, 's');
+			x += 1;
+		};
+	}
+	else
+	{
+		int y = snake->start_y;
+		for (int i = 0; i < snake->blocks; i++)
+		{
+			draw_on_the_screen(screen, snake->start_x, y, 's');
+			y += 1;
+		}
+	}
+}
+int move_snake(struct Snake *snake, struct Screen *screen, char *direction);
+void delete_snake(struct Snake *snake, struct Screen *screen, int col_or_not)
+{
+
+	if (col_or_not == 0)
+	{
+		int x = snake->start_x;
+		for (int i = 0; i < snake->blocks; i++)
+		{
+			draw_on_the_screen(screen, x, snake->start_y, ' ');
+			x += 1;
+		};
+	}
+	else if (col_or_not == 1)
+	{
+		int y = snake->start_y;
+		for (int i = 0; i < snake->blocks; i++)
+		{
+			draw_on_the_screen(screen, snake->start_x, y, ' ');
+			y += 1;
+		}
+	}
+}
 // The entry part
-int main(int argc, char *argv) {
+int main(int argc, char *argv)
+{
 	system("cls");
 	struct Screen scr;
 	struct Snake snake;
-	int x = 0;
+	int default_side = 0;
+	int x = 1;
 	int y = 0;
 	scr = generate_screen(100, 30);
-	get_Snake(&snake, x, y, 1);
+	get_snake(&snake, x, y, 4);
 
-	for (;;){
+	for (;;)
+	{
 		display(&scr);
-		draw_snake(&snake, &scr);
-		if (kbhit() == 1) {
+		draw_snake(&snake, &scr, default_side);
+		if (kbhit() == 1)
+		{
 			char c;
 			c = getch();
-
+			int a;
 			// key 'w' pressed
-			if (c == 119) {
-				move_snake(&snake, &scr, "up");	
-			} 
-			// key 's' pressed
-			else if (c == 115) {
-				move_snake(&snake, &scr, "down");
-			} 
-			// key 'a' pressed
-			else if (c == 97) 
+			if (c == 119)
 			{
-				move_snake(&snake, &scr, "left");
+				a = move_snake(&snake, &scr, "up");
+			}
+			// key 's' pressed
+			else if (c == 115)
+			{
+				a = move_snake(&snake, &scr, "down");
+			}
+			// key 'a' pressed
+			else if (c == 97)
+			{
+				a = move_snake(&snake, &scr, "left");
 			}
 			// key 'd' pressed
-			else if (c == 100) {
-				move_snake(&snake, &scr, "right");
+			else if (c == 100)
+			{
+				a = move_snake(&snake, &scr, "right");
 			}
-					
-		 }
+			default_side = a;
+		}
 	}
-	
-	}
-
+}
 
 // Definition
 
-struct Screen generate_screen(int width, int height) {
+struct Screen generate_screen(int width, int height)
+{
 	struct Screen sam_scr;
 	char empty = ' ';
 
 	sam_scr.width = width;
 	sam_scr.height = height;
-	
-	sam_scr.display = (char *)(malloc(sizeof(char) * ((sam_scr.width * sam_scr.height) + sam_scr.height + 1000)));
-	
-	for (int i = 0; i < sam_scr.height; i++) {
-		if (i == 0) {
-			for (int j = 0; j < sam_scr.width + 1; j++) {
-	
-				if (j == sam_scr.width) {
-					sam_scr.display[(i * sam_scr.width) + j] = '\n';
-				}
-				else {
-					sam_scr.display[(i * sam_scr.width) + j] = empty;
-		
-				}
-		
-			}
 
-		} else {
-			sam_scr.display[(i * sam_scr.width) + 1] = empty;
-			for (int j = 1; j < sam_scr.width + 1; j++) {
-				if (j == sam_scr.width) {
+	sam_scr.display = (char *)(malloc(sizeof(char) * ((sam_scr.width * sam_scr.height) + sam_scr.height + 1000)));
+
+	for (int i = 0; i < sam_scr.height; i++)
+	{
+		if (i == 0)
+		{
+			for (int j = 0; j < sam_scr.width + 1; j++)
+			{
+
+				if (j == sam_scr.width)
+				{
 					sam_scr.display[(i * sam_scr.width) + j] = '\n';
 				}
-				else {
+				else
+				{
 					sam_scr.display[(i * sam_scr.width) + j] = empty;
-		
 				}
-		
-				}
-	
 			}
+		}
+		else
+		{
+			sam_scr.display[(i * sam_scr.width) + 1] = empty;
+			for (int j = 1; j < sam_scr.width + 1; j++)
+			{
+				if (j == sam_scr.width)
+				{
+					sam_scr.display[(i * sam_scr.width) + j] = '\n';
+				}
+				else
+				{
+					sam_scr.display[(i * sam_scr.width) + j] = empty;
+				}
+			}
+		}
 	}
 
 	return sam_scr;
-
 }
 
-void display(struct Screen *scr) {
+void display(struct Screen *scr)
+{
 	system("cls");
 	printf("%s \n", scr->display);
 }
 
-
-void get_Snake(struct Snake *snake, int start_x, int start_y, int blocks) {
-	snake->start_x  = start_x;
+void get_snake(struct Snake *snake, int start_x, int start_y, int blocks)
+{
+	snake->start_x = start_x;
 	snake->start_y = start_y;
 	snake->blocks = blocks;
 	snake->direction = "right";
 }
-void draw_snake(struct Snake *snake, struct Screen *screen) {
-	int x = snake->start_x;
-	for (int i = 0; i < snake->blocks; i++) {
-		draw_on_the_screen(screen, x, snake->start_y, 's');
-		x += 1;
-	};
 
-}
-
-void delete_snake(struct Snake *snake, struct Screen *screen) {
-	int x = snake->start_x;
-	for (int i = 0; i < snake->blocks; i++) {
-		draw_on_the_screen(screen, x, snake->start_y, ' ');
-		x += 1;
-	};
-
-}
-
-void move_snake(struct Snake *snake, struct Screen *screen, char *direction) {
-	if (strcmp(direction, "right") == 0) {
-		if (strcmp(snake->direction, "right") == 0) {
+int move_snake(struct Snake *snake, struct Screen *screen, char *direction)
+{
+	if (strcmp(direction, "right") == 0)
+	{
+		if (strcmp(snake->direction, "right") == 0)
+		{
 			snake->direction = direction;
-			delete_snake(snake, screen);
+			delete_snake(snake, screen, 0);
 			snake->start_x++;
 
-			draw_snake(snake, screen);
-		
+			draw_snake(snake, screen, 0);
 		}
-		else if (strcmp(snake->direction, "left") == 0) {
+		else if (strcmp(snake->direction, "left") == 0)
+		{
 			snake->direction = direction;
-			delete_snake(snake, screen);
+			delete_snake(snake, screen, 0);
 			snake->start_x++;
-			draw_snake(snake, screen);
-		} else if (strcmp(snake->direction, "down") == 0) {
+			draw_snake(snake, screen, 0);
+		}
+		else if (strcmp(snake->direction, "down") == 0)
+		{
 			snake->direction = direction;
-			if (snake->blocks == 1) {
+			if (snake->blocks == 1)
+			{
 				snake->direction = direction;
-				delete_snake(snake, screen);
-				snake->start_y++;
-				draw_snake(snake, screen);
+				delete_snake(snake, screen, 0);
+				snake->start_x++;
+				draw_snake(snake, screen, 0);
+			}
+			else
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 1);
+				snake->start_y += snake->blocks - 1;
+				draw_snake(snake, screen, 0);
 			}
 		}
-		else if (strcmp(snake->direction, "up") == 0) {
+		else if (strcmp(snake->direction, "up") == 0)
+		{
 			snake->direction = direction;
-			if (snake->blocks == 1) {
+			if (snake->blocks == 1)
+			{
 				snake->direction = direction;
-				delete_snake(snake, screen);
-				snake->start_y--;
-				draw_snake(snake, screen);
+				delete_snake(snake, screen, 0);
+				snake->start_x++;
+				draw_snake(snake, screen, 0);
+			}
+			else
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 1);
+				snake->start_y += snake->blocks - 1;
+				draw_snake(snake, screen, 0);
 			}
 		}
-
-	} else if (strcmp(direction, "left") == 0) {
-		if (strcmp(direction, "left") == 0) {
-			snake->direction = direction;
-			delete_snake(snake, screen);
-			snake->start_x--;
-			draw_snake(snake, screen);
-		}
-		else if (strcmp(direction, "right") == 0) {
-			snake->direction = direction;
-			delete_snake(snake, screen);
-			snake->start_x--;
-			draw_snake(snake, screen);
-		} else if (strcmp(snake->direction, "down") == 0) {
-			snake->direction = direction;
-			if (snake->blocks == 1) {
-				snake->direction = direction;
-				delete_snake(snake, screen);
-				snake->start_y++;
-				draw_snake(snake, screen);
-			}
-		}
-		else if (strcmp(snake->direction, "up") == 0) {
-			snake->direction = direction;
-			if (snake->blocks == 1) {
-				snake->direction = direction;
-				delete_snake(snake, screen);
-				snake->start_y--;
-				draw_snake(snake, screen);
-			}
-		}
-
-
-	} else if (strcmp(direction, "down") == 0) {
-		if (strcmp(direction, "right") == 0) {
-			if (snake->blocks == 1) {
-				snake->direction = direction;
-				delete_snake(snake, screen);
-				snake->start_y++;
-				draw_snake(snake, screen);
-			}		
-
-		}
-		else if (strcmp(direction, "left")) {
-			if (snake->blocks == 1) {
-				snake->direction = direction;
-				delete_snake(snake, screen);
-				snake->start_y++;
-				draw_snake(snake, screen);
-			}
-		}
-
 	}
-	else if (strcmp(direction, "up") == 0) {
-		if (strcmp(direction, "right") == 0) {
-			if (snake->blocks == 1) {
-				snake->direction = direction;
-				delete_snake(snake, screen);
-				snake->start_y--;
-				draw_snake(snake, screen);
-			}		
-
+	else if (strcmp(direction, "left") == 0)
+	{
+		if (strcmp(snake->direction, "left") == 0)
+		{
+			snake->direction = direction;
+			delete_snake(snake, screen, 0);
+			snake->start_x--;
+			draw_snake(snake, screen, 0);
 		}
-		else if (strcmp(direction, "left")) {
-			if (snake->blocks == 1) {
+		else if (strcmp(snake->direction, "right") == 0)
+		{
+			snake->direction = direction;
+			delete_snake(snake, screen, 0);
+			snake->start_x--;
+			draw_snake(snake, screen, 0);
+		}
+		else if (strcmp(snake->direction, "down") == 0)
+		{
+			snake->direction = direction;
+			if (snake->blocks == 1)
+			{
 				snake->direction = direction;
-				delete_snake(snake, screen);
-				snake->start_y--;
-				draw_snake(snake, screen);
+				delete_snake(snake, screen, 0);
+				snake->start_x--;
+				draw_snake(snake, screen, 0);
+			}
+			else
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 1);
+				snake->start_y += snake->blocks - 1;
+				draw_snake(snake, screen, 0);
 			}
 		}
-
+		else if (strcmp(snake->direction, "up") == 0)
+		{
+			snake->direction = direction;
+			if (snake->blocks == 1)
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 0);
+				snake->start_x--;
+				draw_snake(snake, screen, 0);
+			}
+			else
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 1);
+				snake->start_y += snake->blocks - 1;
+				draw_snake(snake, screen, 0);
+			}
+		}
 	}
+	else if (strcmp(direction, "down") == 0)
+	{
+		if (strcmp(snake->direction, "right") == 0)
+		{
+			if (snake->blocks == 1)
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 0);
+				if (snake->start_y == 0)
+				{
+					snake->start_y += 2;
+				}
+				else
+				{
+					snake->start_y++;
+				}
+				snake->start_x += snake->blocks - 1;
+				draw_snake(snake, screen, 0);
+			}
+			else
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 0);
+				if (snake->start_y == 0)
+				{
+					snake->start_y += 2;
+				}
+				else
+				{
+					snake->start_y++;
+				}
+				snake->start_x += snake->blocks - 1;
+				draw_snake(snake, screen, 1);
+			}
+			return 1;
+		}
+		else if (strcmp(snake->direction, "left") == 0)
+		{
+			if (snake->blocks == 1)
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 0);
+				if (snake->start_y == 0)
+				{
+					snake->start_y += 2;
+				}
+				else
+				{
+					snake->start_y++;
+				}
+				draw_snake(snake, screen, 0);
+			}
+			else
+			{
 
+				snake->direction = direction;
+				delete_snake(snake, screen, 0);
+				if (snake->start_y == 0)
+				{
+					snake->start_y += 2;
+				}
+				else
+				{
+					snake->start_y++;
+				}
+				draw_snake(snake, screen, 1);
+			}
+			return 1;
+		}
+		else if (strcmp(snake->direction, "down") == 0)
+		{
+			snake->direction = direction;
+			delete_snake(snake, screen, 1);
+			if (snake->start_y == 0)
+			{
+				snake->start_y += 2;
+			}
+			else
+			{
+				snake->start_y++;
+			}
+			draw_snake(snake, screen, 1);
+			return 1;
+		}
+		else if (strcmp(snake->direction, "up") == 0)
+		{
+			snake->direction = direction;
+			delete_snake(snake, screen, 1);
+			if (snake->start_y == 0)
+			{
+				snake->start_y += 2;
+			}
+			else
+			{
+				snake->start_y++;
+			}
+			draw_snake(snake, screen, 1);
+			return 1;
+		}
+	}
+	else if (strcmp(direction, "up") == 0)
+	{
+		if (strcmp(snake->direction, "right") == 0)
+		{
+			if (snake->blocks == 1)
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 0);
+				if (snake->start_y == 0)
+				{
+					snake->start_y -= 2;
+				}
+				else
+				{
+					snake->start_y--;
+				}
+				snake->start_x += snake->blocks - 1;
+				draw_snake(snake, screen, 0);
+			}
+			else
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 0);
+				if (snake->start_y == 0)
+				{
+					snake->start_y -= 2;
+				}
+				else
+				{
+					snake->start_y--;
+				}
+				snake->start_x += snake->blocks - 1;
+				draw_snake(snake, screen, 1);
+			}
+			return 1;
+		}
+		else if (strcmp(snake->direction, "left") == 0)
+		{
+			if (snake->blocks == 1)
+			{
+				snake->direction = direction;
+				delete_snake(snake, screen, 0);
+				if (snake->start_y == 0)
+				{
+					snake->start_y -= 2;
+				}
+				else
+				{
+					snake->start_y--;
+				}
+				draw_snake(snake, screen, 0);
+			}
+			else
+			{
+
+				snake->direction = direction;
+				delete_snake(snake, screen, 0);
+				if (snake->start_y == 0)
+				{
+					snake->start_y -= 2;
+				}
+				else
+				{
+					snake->start_y--;
+				}
+				draw_snake(snake, screen, 1);
+			}
+			return 1;
+		}
+		else if (strcmp(snake->direction, "down") == 0)
+		{
+			snake->direction = direction;
+			delete_snake(snake, screen, 1);
+			if (snake->start_y == 0)
+			{
+				snake->start_y -= 2;
+			}
+			else
+			{
+				snake->start_y--;
+			}
+			draw_snake(snake, screen, 1);
+			return 1;
+		}
+		else if (strcmp(snake->direction, "up") == 0)
+		{
+			snake->direction = direction;
+			delete_snake(snake, screen, 1);
+			if (snake->start_y == 0)
+			{
+				snake->start_y -= 2;
+			}
+			else
+			{
+				snake->start_y--;
+			}
+			draw_snake(snake, screen, 1);
+			return 1;
+		}
+	}
+	return 0;
 }
-
